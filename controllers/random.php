898ae8335c6e81789ad13167bcbbbe9dbf8cf41c
TylerClare <56036789+TylerClare@users.noreply.github.com>
2019-12-11 15:02:15 -0500

    Update random.php

--- controllers/random.php
+++ controllers/random.php
@@ -5,6 +5,10 @@ include('../views/heading.php');
 include('../models/DBConnect.php');
 include('../models/DBFunctions.php');
 
+if (!isset($_SESSION['signedInEmail'])){
+        header("Location: login.php");
+}
+
 $redirect = RandomDeck();
 
 header('Location: ' . $redirect);
