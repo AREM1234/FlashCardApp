b0261ecd5c000fefeedc7ebc44b4933a7a9a9e06
TylerClare <56036789+TylerClare@users.noreply.github.com>
2019-12-11 15:02:48 -0500

    Update removeDeck.php

--- controllers/removeDeck.php
+++ controllers/removeDeck.php
@@ -4,6 +4,10 @@ session_start();
 include('../models/DBConnect.php');
 include('../models/DBFunctions.php');
 
+if (!isset($_SESSION['signedInEmail'])){
+        header("Location: login.php");
+}
+
 if(!isset($_GET['DeckID'])){
 	exit();
 }
