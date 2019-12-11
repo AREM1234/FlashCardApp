6ceebc7baa8a4a2076d750a320c5743733cb8990
Samuel Lieber <36018073+samueljlieber@users.noreply.github.com>
2019-12-11 12:09:26 -0500

    Fixed a scrolling bounds issue

--- views/navigation.php
+++ views/navigation.php
@@ -2,9 +2,9 @@
     session_start();
     include('heading.php');
 ?>
-<div class="container-fluid container-img">
-    <div class="row text-center">
-        <div class="col-12 my-2 p-0">
+<div class="container-fluid container-img dis-none" id="slide-out-action">
+    <div class="row m-0 p-0">
+        <div class="col-12 m-0 p-0">
             <div class="row m-0 p-0">
                 <div class="col-6 text-left p-3">
                     <form action="../controllers/logout.php">
@@ -16,15 +16,15 @@
                 </div>
             </div>
         </div>
-        <div class="col-12 my-3">
+        <div class="col-12 my-3 text-center">
             <img src="../images/alfred-state-a.png" class="img-fluid alfred-logo" alt="Alfred State Logo">
             <h1>Flashcard Application Menu</h1>            
         </div>
         <div class="col-sm-8 col-md-6 col-lg-4 mt-5 mx-auto">
-            <a href="../continueLearning.php" class="primary-btn">Continue Learning</a>
-            <a href="../controllers/addDeck.php" class="primary-btn">Create a New rfsvdsd</a>
+            <a href="../views/continueLearning.php" class="primary-btn">Continue Learning</a>
+            <a href="../controllers/addDeck.php" class="primary-btn">Create a New Deck</a>
             <a href="../views/mydecks.php" class="primary-btn">My Decks</a>
-            <a href="/controllers/random.php" class="primary-btn">Random Flashcard</a>
+            <a href="../controllers/random.php" class="primary-btn">Random Deck</a>
         </div>
     </div>
 </div>
