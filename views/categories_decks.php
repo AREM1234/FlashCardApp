fc86ba49c42109c8c80cd18d492808ea0f150b7f
TylerClare <56036789+TylerClare@users.noreply.github.com>
2019-12-11 15:04:05 -0500

    Update categories_decks.php

--- views/categories_decks.php
+++ views/categories_decks.php
@@ -5,6 +5,10 @@ include('heading.php');
 include('../models/DBConnect.php');
 include('../models/DBFunctions.php');
 
+if (!isset($_SESSION['signedInEmail'])){
+        header("Location: ../controllers/login.php");
+}
+
 if(isset($_GET['categoryID'])){
 	$id = $_GET['categoryID'];
 }
